9d7acc12a3f394072cfcd6dcb2abcf9f3eda2f7e
okborges <rafaelborges.av@gmail.com>
2023-04-20 11:00:37 -0300

    title

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,22 +1,31 @@
 <template>
-  <section>
-    <div></div>
+  <section class="p-5 bg-black">
+    <div class="grid w-full h-full place-content-center text-center text-white">
+      <h1 class="font-semibold text-8xl pb-7">Desenvolvedor Front End</h1>
+      <h2 class="text-2xl p-4 rounded-2xl justify-self-center">
+        Apaixonado por: Design, Natureza, Esportes radicais, Cultura brasileira, Uma boa fofoca.
+      </h2>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
 section {
   height: 100svh;
-  padding: 20px;
-  background-color: #000000;
   div {
-    width: 100%;
-    height: 100%;
     background-image: url(../assets/img/giga.webp);
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
     border-radius: 45px;
+
+    h2 {
+      background-color: rgb(0 0 0 / 0.7);
+      transition: 0.2s;
+    }
+    h2:hover {
+      background-color: rgb(0 0 0 / 1);
+    }
   }
 }
 </style>
