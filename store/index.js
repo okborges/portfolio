631d47983964e0eadb12a02fc666de26493c56f4
okborges <rafaelborges.av@gmail.com>
2023-05-17 18:37:50 -0300

    store folder

---
+++ store/index.js
@@ -0,0 +1,54 @@
+const techs = [
+  {
+    nome: 'Vue.js',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(65, 184, 131, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(65, 184, 131, 0.5) 100%)'
+  },
+  {
+    nome: 'Nuxt',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(0, 255, 151, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(0, 255, 151, 0.5) 100%)'
+  },
+  {
+    nome: 'Web Desing',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(0, 150, 157, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(0, 150, 157, 0.5) 100%)'
+  },
+  {
+    nome: 'Figma',
+    icon: '',
+    bg: 'linear-gradient(90deg,   rgba(242, 78, 30, 0.5) 0%, rgba(162, 89, 255, 0.5) 6%, rgba(26, 188, 254, 0.5) 12%, rgba(10, 207, 131, 0.5) 18%, rgba(10, 207, 131, 0) 30%, rgba(10, 207, 131, 0) 70%, rgba(10, 207, 131, 0.5) 82%, rgba(26, 188, 254, 0.5) 88%, rgba(162, 89, 255, 0.5) 94%, rgba(242, 78, 30, 0.5) 100%)'
+  },
+  {
+    nome: 'JavaScript',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(247, 223, 30, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(247, 223, 30, 0.5) 100%)'
+  },
+  {
+    nome: 'Tailwind',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(24, 181, 185, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(24, 181, 185, 0.5) 100%)'
+  },
+  {
+    nome: 'HTML',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(227, 78, 38, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(227, 78, 38, 0.5) 100%)'
+  },
+  {
+    nome: 'CSS',
+    icon: '',
+    bg: 'linear-gradient(90deg, rgba(28, 136, 199, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%, rgba(28, 136, 199, 0.5) 100%)'
+  },
+  {
+    nome: 'GIT',
+    icon: '',
+    bg: 'linear-gradient(90deg,rgba(66, 0, 152, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%,rgba(66, 0, 152, 0.5) 100%)'
+  },
+  {
+    nome: 'React',
+    icon: '',
+    bg: 'linear-gradient(90deg,rgba(83, 193, 222, 0.5) 0%, rgba(65, 184, 131, 0) 30%, rgba(65, 184, 131, 0) 70%,rgba(83, 193, 222, 0.5) 100%)'
+  }
+];
+
+export default techs;
